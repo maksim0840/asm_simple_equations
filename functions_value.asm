d5c7a04b3658cca7cc386ee47eb6065ef297461b
maksim0840 <maksim0840@gmail.com>
2024-06-08 17:10:08 +0300

    calculating ((x - 2)^3 - 1) function

--- functions_value.asm
+++ functions_value.asm
@@ -2,10 +2,13 @@ extern printf
 
 section .rodata
     format_double: db "%f", 0
+    x: dq -15.416
     
-    x: dq 19.5
     const_f1_1: dq 0.6
     const_f1_2: dq 3.0
+    
+    const_f2_1: dq 2.0
+    const_f2_2: dq 1.0
 
 section .bss
     result: resq 1
@@ -15,7 +18,7 @@ global main
 main:
     push dword[x + 4]
     push dword[x]
-    call f1_value
+    call f2_value
     pop dword[result]
     pop dword[result + 4]
     
@@ -29,12 +32,11 @@ main:
     xor eax, eax
     ret
     
-; 0.6*x + 3
+; (0.6*x + 3) function
 f1_value:
     push ebp
     mov ebp, esp
     
-    mov eax, dword[ebp + 8] ; x
     finit
     
     fld qword[ebp + 8] ; x
@@ -48,6 +50,23 @@ f1_value:
 
 ; (x - 2)^3 - 1
 f2_value:
-
+    push ebp
+    mov ebp, esp
+    
+    finit
+    
+    fld qword[ebp + 8] ; x
+    fsub qword[const_f2_1] ; x - 2
+    fld st0 ; clone (x - 2)
+    fld st0 ; clone (x - 2)
+    fmulp ; (x - 2)^2
+    fmulp ; (x - 2)^3
+    fsub qword[const_f2_2] ; (x - 2)^3 - 1
+    
+    fstp qword[ebp + 8]
+   
+    pop ebp
+    ret
+    
 ; 3 / x
 f3_value:

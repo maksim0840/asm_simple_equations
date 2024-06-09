bee79e93e66a72c12295e4881cec88a797d4b401
maksim0840 <maksim0840@gmail.com>
2024-06-09 12:26:07 +0300

    call ASM functions from C

--- integral.c
+++ integral.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
+extern double f1_value(double x);
+extern double f2_value(double x);
+extern double f3_value(double x);
+
 int main(int argc, char* argv[]) {
-   printf("Hello, world!\n");
+   printf("%f\n", f1_value(5.4));
+   printf("%f\n", f2_value(5.4));
+   printf("%f\n", f3_value(5.4));
    return 0;
 }

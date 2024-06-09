855eb0fc980bd8ea4acb2dbedcdb9c99bdfc83ee
maksim0840 <maksim0840@gmail.com>
2024-06-09 12:23:29 +0300

    cdecl functions

--- functions_value.asm
+++ functions_value.asm
@@ -1,9 +1,5 @@
-extern printf
 
 section .rodata
-    format_double: db "%f", 0
-    x: dq 0.03
-    
     const_f1_1: dq 0.6
     const_f1_2: dq 3.0
     
@@ -12,50 +8,37 @@ section .rodata
     
     const_f3_1: dq 3.0
 
+
 section .bss
     result: resq 1
-    
+
+        
 section .text
-global main
-main:
-    push dword[x + 4]
-    push dword[x]
-    call f3_value
-    pop dword[result]
-    pop dword[result + 4]
-    
-    sub esp, 4
-    push dword[result + 4]
-    push dword[result]
-    push format_double
-    call printf
-    add esp, 16
-    
-    xor eax, eax
-    ret
-    
+global f1_value, f2_value, f3_value
+
 ; (0.6*x + 3) function
 f1_value:
     push ebp
     mov ebp, esp
     
-    finit
+    finit ; start and clear x87
     
     fld qword[ebp + 8] ; x
     fmul qword[const_f1_1] ; 0.6*x
     fadd qword[const_f1_2] ; 0.6*x + 3
     
-    fstp qword[ebp + 8]
-   
+    ; value returned in st0
+    
     pop ebp
     ret
+
 
 ; (x - 2)^3 - 1
 f2_value:
     push ebp
     mov ebp, esp
     
-    finit
+    finit ; start and clear x87
     
     fld qword[ebp + 8] ; x
     fsub qword[const_f2_1] ; x - 2
@@ -65,22 +48,23 @@ f2_value:
     fmulp ; (x - 2)^3
     fsub qword[const_f2_2] ; (x - 2)^3 - 1
     
-    fstp qword[ebp + 8]
+    ; value returned in st0
    
     pop ebp
     ret
-    
+
+        
 ; 3 / x
 f3_value:
     push ebp
     mov ebp, esp
     
-    finit
+    finit ; start and clear x87
     
     fld qword[const_f3_1] ; 3
     fdiv qword[ebp + 8] ; 3 / x
  
-    fstp qword[ebp + 8]
+    ; value returned in st0
     
     pop ebp
     ret
